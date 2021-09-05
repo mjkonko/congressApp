f021118078c47e37fe6932953687219b77c01144
mjkonko <mjkonko@gmail.com>
2021-09-05 18:27:45 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/polishcongress/congressApp/polish_congress_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.polishcongress.congressApp.polish_congress_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/agenda.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Agenda extends StatefulWidget {
+  Agenda({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _AgendaState createState() => _AgendaState();
+
+}
+
+class _AgendaState extends State<Agenda> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+  }
