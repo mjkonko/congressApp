3d570730c1f29d8490c8a0f84d10d77ec1a95b77
mikki <mikki@nenna.is>
2022-01-04 18:24:03 +0100

    Updated news

--- lib/entity/NewsItem.dart
+++ lib/entity/NewsItem.dart
@@ -1,22 +1,23 @@
 class NewsItem {
   final int id;
+  final String time;
   final String title;
   final String body ;
-  final String time;
 
   NewsItem({
     required this.id,
+    required this.time,
     required this.title,
     required this.body,
-    required this.time
+
   });
 
   factory NewsItem.fromJson(Map<String, dynamic> json) {
     return NewsItem(
         id: json['id'],
+        time: json['time'],
         title: json['title'],
-        body: json['body'],
-        time: json['time']
+        body: json['body']
     );
   }
 }
--- lib/start.dart
+++ lib/start.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:polish_congress_app/globals.dart';
-import 'package:polish_congress_app/entity/User.dart';
 
 import 'entity/NewsItem.dart';
 
